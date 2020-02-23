c52ff21350f01aea2c15e838bc8a045c5f4ca6b0
Jeff <jeff.sagal@briteweb.com>
2020-02-23 13:39:19 -0800

    Updates

---
+++ precache-manifest.a7f371df249483be375d4f5f610089da.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddf25ea37cfcdd18b3e0",
+    "url": "/css/app.c5651706.css"
+  },
+  {
+    "revision": "ddf25ea37cfcdd18b3e0",
+    "url": "/js/app.9c74d1ee.js"
+  },
+  {
+    "revision": "ce53039c7ad2361862dc",
+    "url": "/js/chunk-vendors.c8e7eeaf.js"
+  },
+  {
+    "revision": "7d1687ed610d82491930585fdb168bde",
+    "url": "/index.html"
+  }
+];
